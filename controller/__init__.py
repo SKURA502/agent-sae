1b7747ce9a2c7a15080b76bcde8a79a3d6fb7537
SKURA502 <ziheng.peng@ruc.edu.cn>
2026-05-04 21:56:27 +0800

    ok

--- controller/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Controller 模块 - 最小 Agent Loop 和 Sandbox Tools（H2 rollout 生成）"""
-
-from .agent_loop import AgentConfig, AgentLoop, EpisodeStep
-from .sandbox_tools import SandboxToolKit
-
-__all__ = ["AgentConfig", "AgentLoop", "EpisodeStep", "SandboxToolKit"]
